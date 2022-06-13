078d530da74aac7f7d3a8538e6ac30ca8a4a12ea
Agnel Nieves <agnelnieves@gmail.com>
2022-06-12 23:43:29 -0400

    feat(chapter_01): Hello world

---
+++ ch01-02_hello_world/main.rs
@@ -0,0 +1,5 @@
+//  main is the first code that runs in every executable Rust program
+fn main() {
+    println!("Hello, world!");
+    //    /! = calls a Rust macro. If it called a function instead, it would be entered as println (without the !).
+}
---
+++ ch01-02_hello_world/readme.md
@@ -0,0 +1,8 @@
+enter the following commands to compile and run the file:
+
+```rust
+rustc main.rs // Compiles and generates the binary executable
+./main // executing the file in the current directory
+
+Hello, world! // output
+```
